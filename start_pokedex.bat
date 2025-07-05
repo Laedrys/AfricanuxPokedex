d1ad1e5a22c7f8cef6e62e2b2aa5ac9bb0305566
Julien SCHAMETZ <slayder972@gmail.com>
2025-07-05 02:39:50 +0200

    Mise à jour locale vers la branche feature

---
+++ start_pokedex.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Démarrage de l'application Africanus Pokédex...
+REM Active ton environnement virtuel si besoin :
+REM call venv\Scripts\activate
+
+REM Lance l'application
+python app.py
+
+pause
